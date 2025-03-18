d8ac7017983da1b468a83c4698f311c2323860af
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-03-18 14:11:16 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_2311104073/HaloGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine($"Halo user {nama}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Farhan");  // Ganti dengan nama panggilanmu
+
+        Console.ReadKey();
+    }
+}
